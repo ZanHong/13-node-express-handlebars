2bf42c6193be5ec1635aef165a6eab72709d5f87
Zan Hong <mokzanhong92@hotmail.com>
2020-08-13 17:08:46 +0930

    Complete DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Chicken McSpicy Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Beef Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Patty");
